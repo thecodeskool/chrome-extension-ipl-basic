02bf34ee093a9ee6f0b580cfee88d79a3591029b
thecodeskool <72975267+thecodeskool@users.noreply.github.com>
2023-04-29 14:21:25 +0530

    Added files for chrome extension

---
+++ script.js
@@ -0,0 +1,24 @@
+async function getMatchData() {
+
+    return await fetch("https://api.cricapi.com/v1/currentMatches?apikey=bef4b6e4-0fbb-4e11-9410-6e16e2ca5ab1&offset=0")
+        .then(data => data.json())
+        .then(data => {
+            if (data.status != "success")return;
+
+            const matchesList = data.data;
+
+            if(!matchesList)return [];
+            
+            const relevantData = matchesList.filter(match => match.series_id == "c75f8952-74d4-416f-b7b4-7da4b4e3ae6e").map(match => `${match.name}, ${match.status}`);
+
+            console.log({relevantData});
+
+            document.getElementById("matches").innerHTML = relevantData.map(match => `<li>${match} </li>`).join('');
+
+            return relevantData;
+
+        })
+        .catch(e => console.log(e));
+}
+
+getMatchData();
